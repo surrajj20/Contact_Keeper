20ac4171d7f79192a7fed1d2ec2e513efd78bf0e
surrajj20 <shah.suraj1200@gmail.com>
2020-02-29 01:58:32 +0530

    about page updated & server,package-jason,production.json file updated

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,10 +5,19 @@ const About = () => {
         <div>
             <h1>About This App</h1>
             <p className="my-1">
-                This is a full stack React app for keeping contacts
+                This is a full stack React app for keeping contacts<br />
+                Design By:- Suraj Sah<br />
+                BTech CSE 2nd year<br />
+                USICT,GGSIPU<br />
+                <p className="bg-dark p">
+                    <strong> Github-Id </strong> https://github.com/surrajj20 <br />
+                    <strong> LinkedIn-Id </strong> https://www.linkedin.com/in/shahsuraj1200/ <br />
+
+                </p>
+
             </p>
             <p className="bg-dark p">
-                <strong> Version: </strong> 1.0.0
+                <strong> Version: </strong> 1.1.1
             </p>
 
         </div>
